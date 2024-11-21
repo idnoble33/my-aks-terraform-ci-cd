b8aae513dd31fee8786eb7b87801823e7f0ed953
Idowu Bello <noble@Idowus-MacBook-Pro.local>
2024-11-20 21:27:14 -0700

    update the resoures

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,77 +1,9 @@
-# provider "azurerm" {
-#   features {}
-# }
-
-# data "azurerm_resource_group" "current" {
-#   name = var.resource_group_name
-# }
-
-# # Resource Group
-# resource "azurerm_resource_group" "candidate_rg" {
-#   name     = var.resource_group_name
-#   location = var.location
-# }
-
-# # ACR Module
-# module "acr" {
-#   source              = "./modules/acr"
-#   acr_name            = "myacr"                          # Replace with your ACR name
-#   resource_group_name = azurerm_resource_group.candidate_rg.name
-#   location            = azurerm_resource_group.candidate_rg.location
-#   admin_enabled       = true
-# }
-
-# # VM Module
-# module "vm" {
-#   source              = "./modules/vm"
-#   vm_name             = var.vm_name
-#   location            = azurerm_resource_group.candidate_rg.location
-#   resource_group_name = azurerm_resource_group.candidate_rg.name
-#   vm_size             = var.vm_size
-#   admin_user          = var.admin_user
-#   ssh_key_path        = var.ssh_key_path
-#   subnet_id           = var.subnet_id
-# }
-
-# # Key Vault Module
-# module "keyvault" {
-#   source              = "./modules/keyvault"
-#   keyvault_name       = var.keyvault_name
-#   location            = azurerm_resource_group.candidate_rg.location
-#   resource_group_name = azurerm_resource_group.candidate_rg.name
-#   secret_name         = var.secret_name
-#   secret_value        = var.secret_value
-#   tenant_id           = var.tenant_id
-# }
-
-# # AKS Module
-# module "aks" {
-#   source              = "./modules/aks"
-#   aks_name            = var.aks_name
-#   location            = azurerm_resource_group.candidate_rg.location
-#   resource_group_name = azurerm_resource_group.candidate_rg.name
-#   dns_prefix          = var.dns_prefix
-#   vm_size             = var.aks_vm_size
-#   node_count          = var.aks_node_count
-#   extra_pool_vm_size  = var.extra_pool_vm_size
-#   extra_pool_node_count = var.extra_pool_node_count
-# }
-
-# resource "null_resource" "update_inventory" {
-#   depends_on = [module.vm]  # Ensures the VM is fully provisioned before this runs
-
-#   provisioner "local-exec" {
-#     command = <<EOT
-#     echo "[jenkins_server]" > ${path.module}/../ansible/inventory.ini
-#     echo "${module.vm.public_ip} ansible_user=azureuser ansible_ssh_private_key_file=${var.ssh_key_path}" >> ${path.module}/../ansible/inventory.ini
-#     EOT
-#   }
-# }
 
 provider "azurerm" {
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
   features        {}
+  resource_provider_registrations = "none" 
 }
 
 # Resource Group
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,14 +1,16 @@
 # VM and Resource Group Configuration
 vm_name              = "my-vm"                          # Name for the virtual machine
-resource_group_name  = "Idowu-Candidate"              # Resource group name
-location             = "West Europe"                        # Azure region
-vm_size              = "Standard_B1s"                  # VM size (adjust as per your needs)
+resource_group_name  = "Idowu-Candidate"                # Resource group name
+location             = "West Europe"                    # Azure region
+vm_size              = "Standard_B1s"                   # VM size (adjust as per your needs)
 admin_user           = "azureuser"                      # The admin username for SSH login
 
 # Network Configuration
-subnet_id            = "/subscriptions/27c83813-916e-49fa-8d2a-d35332fc8ca4/resourceGroups/my-resource-group/providers/Microsoft.Network/virtualNetworks/my-vnet/subnets/my-subnet"  # Example subnet ID
+subnet_id            = "/subscriptions/2fa0e512-f70e-430f-9186-1b06543a848e/resourceGroups/Idowu-Candidate/providers/Microsoft.Network/virtualNetworks/my-vnet/subnets/my-subnet"
 
 # SSH Public Key Path
-subscription_id = "27c83813-916e-49fa-8d2a-d35332fc8ca4"
-ssh_key_path = "/Users/noble/.ssh/id_rsa.pub"
-tenant_id = "27c83813-916e-49fa-8d2a-d35332fc8ca4"
+ssh_key_path         = "/Users/noble/.ssh/id_rsa.pub"    # Path to SSH public key
+
+# Subscription and Tenant IDs
+subscription_id      = "2fa0e512-f70e-430f-9186-1b06543a848e"  # Subscription ID
+tenant_id            = "2fa0e512-f70e-430f-9186-1b06543a848e"    # Tenant ID
